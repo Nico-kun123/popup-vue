5531e322f79ec23ab22ece5900c4d9a8421f44d8
Nicolay Kudryavtsev <nicolay.kudryavtsev@gmail.com>
2024-04-01 00:26:21 +0700

    small fix

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  optimizeDeps: {
+    include: ['vue'],
+  },
+  base: './',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 })
